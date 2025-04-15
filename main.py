a9c7c097d15d088ddbfaf1dc8e46c28c8fed9d02
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-04-15 21:34:37 +0900

    About to test the Car class out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from car import Car
 
 WIDTH, HEIGHT = 800, 700
 FPS = 60
@@ -10,7 +11,8 @@ def main():
     pygame.display.set_caption("Self-Driving Car - DQN")
     clock = pygame.time.Clock()
 
-    track = pygame.image.load("assets/new_map.png")
+    track = pygame.image.load("assets/new_map.png").convert()
+    car = Car(420, 640)
 
     running = True
     while running:
@@ -20,11 +22,18 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            car.rotate(5)
+        if keys[pygame.K_RIGHT]:
+            car.rotate(-5)
+
+        car.move()
+        car.update_sensors(track)
+        car.draw(screen)
+
         pygame.display.flip()
         clock.tick(FPS)
 
     pygame.quit()
     sys.exit()
-
-if __name__ == "__main__":
-    main()
